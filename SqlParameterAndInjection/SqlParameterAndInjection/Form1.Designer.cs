02853dd96f3fc27a7d3a21f9bfe64c33cb20166e
victorioustr <makyil@gmail.com>
2019-03-02 04:05:53 +0300

    SqlParameterAndInjection

---
+++ SqlParameterAndInjection/SqlParameterAndInjection/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace SqlParameterAndInjection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDataSource = new System.Windows.Forms.TextBox();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtInitialCatalog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnKontrolEt = new System.Windows.Forms.Button();
+            this.rbStringFormat = new System.Windows.Forms.RadioButton();
+            this.rbSqlParameter = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtDataSource
+            // 
+            this.txtDataSource.Location = new System.Drawing.Point(148, 33);
+            this.txtDataSource.Name = "txtDataSource";
+            this.txtDataSource.Size = new System.Drawing.Size(174, 26);
+            this.txtDataSource.TabIndex = 1;
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(148, 65);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(174, 26);
+            this.txtUserId.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(148, 97);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(174, 26);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // txtInitialCatalog
+            // 
+            this.txtInitialCatalog.Location = new System.Drawing.Point(148, 129);
+            this.txtInitialCatalog.Name = "txtInitialCatalog";
+            this.txtInitialCatalog.Size = new System.Drawing.Size(174, 26);
+            this.txtInitialCatalog.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Initial Catalog";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBaglan);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtDataSource);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtUserId);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtInitialCatalog);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 203);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Veritabanı Bilgileri";
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Location = new System.Drawing.Point(148, 161);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(174, 30);
+            this.btnBaglan.TabIndex = 8;
+            this.btnBaglan.Text = "Bağlan";
+            this.btnBaglan.UseVisualStyleBackColor = true;
+            this.btnBaglan.Click += new System.EventHandler(this.btnBaglan_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(117, 129);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(174, 26);
+            this.txtPass.TabIndex = 5;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(117, 97);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(174, 26);
+            this.txtUser.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Username";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 20);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Password";
+            // 
+            // btnKontrolEt
+            // 
+            this.btnKontrolEt.Location = new System.Drawing.Point(117, 161);
+            this.btnKontrolEt.Name = "btnKontrolEt";
+            this.btnKontrolEt.Size = new System.Drawing.Size(174, 30);
+            this.btnKontrolEt.TabIndex = 6;
+            this.btnKontrolEt.Text = "Kontrol Et";
+            this.btnKontrolEt.UseVisualStyleBackColor = true;
+            this.btnKontrolEt.Click += new System.EventHandler(this.btnKontrolEt_Click);
+            // 
+            // rbStringFormat
+            // 
+            this.rbStringFormat.AutoSize = true;
+            this.rbStringFormat.Checked = true;
+            this.rbStringFormat.Location = new System.Drawing.Point(16, 35);
+            this.rbStringFormat.Name = "rbStringFormat";
+            this.rbStringFormat.Size = new System.Drawing.Size(131, 24);
+            this.rbStringFormat.TabIndex = 0;
+            this.rbStringFormat.TabStop = true;
+            this.rbStringFormat.Text = "String.Format";
+            this.rbStringFormat.UseVisualStyleBackColor = true;
+            // 
+            // rbSqlParameter
+            // 
+            this.rbSqlParameter.AutoSize = true;
+            this.rbSqlParameter.Location = new System.Drawing.Point(165, 35);
+            this.rbSqlParameter.Name = "rbSqlParameter";
+            this.rbSqlParameter.Size = new System.Drawing.Size(131, 24);
+            this.rbSqlParameter.TabIndex = 1;
+            this.rbSqlParameter.Text = "SqlParameter";
+            this.rbSqlParameter.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbStringFormat);
+            this.groupBox2.Controls.Add(this.rbSqlParameter);
+            this.groupBox2.Controls.Add(this.txtPass);
+            this.groupBox2.Controls.Add(this.txtUser);
+            this.groupBox2.Controls.Add(this.btnKontrolEt);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(383, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 203);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sql Injection Kontrolü";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(388, 243);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "\'or 1=1--";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(369, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Sql Injection Örneği ( Password kutusuna yapıştırın)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 292);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL Injection Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDataSource;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtInitialCatalog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnKontrolEt;
+        private System.Windows.Forms.RadioButton rbStringFormat;
+        private System.Windows.Forms.RadioButton rbSqlParameter;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SqlParameterAndInjection/SqlParameterAndInjection/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SqlParameterAndInjection
+{
+    /// <summary>
+    /// SqlParameter kullanımı ve Sql Injection'la Mücadele
+    /// Author : Muzaffer Ali AKYIL 
+    /// Email : muzaffer@akyil.net
+    /// Web : https://muzaffer.akyil.net
+    /// Date : 02.03.2019 04:04
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnBaglan.Tag = false;
+        }
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+            // Burada button'un Tag özelliğine butonun durumunu ifade eden bir bool değer kullanıyoruz. Böylece button'un o anki durumunu öğrenebiliyoruz.
+            if ((bool)btnBaglan.Tag)
+            {
+                if (connection.State != System.Data.ConnectionState.Closed)
+                    connection.Close();
+                btnBaglan.Text = "Bağlan";
+                btnBaglan.Tag = false;
+                return;
+            }
+
+            // Database var mı diye kontrol etmek için InitialCatalog vermeden yani master db ye bağlanıp ilgili Database var mı sorgusunu atıyoruz.
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = txtDataSource.Text,
+                UserID = txtUserId.Text,
+                Password = txtPassword.Text
+            };
+
+            try
+            {
+                using (SqlConnection checkDbConnection = new SqlConnection(connectionStringBuilder.ConnectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("SELECT db_id(@dbname)", checkDbConnection))
+                    {
+                        command.Parameters.AddWithValue("@dbname", txtInitialCatalog.Text);
+                        checkDbConnection.Open();
+                        if (command.ExecuteScalar() == DBNull.Value)
+                        {
+                            if (MessageBox.Show($"{txtInitialCatalog.Text} isimli veritabanı bulunamadı. Oluşturulsun mu ?", "Veritabanı Bulunamadı", MessageBoxButtons.YesNo) == DialogResult.No)
+                                return;
+                            // Database yok sa oluşturmak için sql cümlelerini çalıştıran methodu çağırıyoruz.
+                            CreateDatabase(checkDbConnection);
+                        }
+                        // Database zaten var ya da yukarıda oluşturduğumuz için global olarak tanımladığımız connection nesnesini oluştururken InitialCatalog u db ismimiz ile değiştiriyoruz.
+                        connectionStringBuilder.InitialCatalog = txtInitialCatalog.Text;
+                        connection = new SqlConnection(connectionStringBuilder.ConnectionString);
+                        connection.Open();
+                    }
+                    btnBaglan.Tag = true;
+                    btnBaglan.Text = "Bağlantıyı Kes";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Varitabanına bağlanırken sorun yaşandı :\r\n{ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CreateDatabase(SqlConnection testConnection)
+        {
+            string sqlQuery = $@"CREATE DATABASE [{txtInitialCatalog.Text}];";
+            string sqlTableQuery = $@"
+USE [{txtInitialCatalog.Text}];
+CREATE TABLE [dbo].[Users](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY];
+INSERT INTO Users (Username,Password,Email) VALUES ('Ahmet', '12345', 'a@admin.com');
+INSERT INTO Users (Username,Password,Email) VALUES ('Mehmet', 'qwerty', 'b@admin.com');
+INSERT INTO Users (Username,Password,Email) VALUES ('Zeynep', 'asdfgh', 'c@admin.com');
+INSERT INTO Users (Username,Password,Email) VALUES ('Mahmut', 'zxcvbn', 'd@admin.com');
+";
+
+            using (SqlCommand command = new SqlCommand(sqlQuery, testConnection))
+            {
+                if (testConnection.State != System.Data.ConnectionState.Open)
+                    testConnection.Open();
+                command.ExecuteNonQuery();
+            }
+            using (SqlCommand command = new SqlCommand(sqlTableQuery, testConnection))
+            {
+                if (testConnection.State != System.Data.ConnectionState.Open)
+                    testConnection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void btnKontrolEt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(connection == null)
+                {
+                    MessageBox.Show("Lütfen önce veritabanına bağlanın.");
+                    return;
+                }
+
+                if (connection.State != System.Data.ConnectionState.Open)
+                    connection.Open();
+
+                if (rbStringFormat.Checked)
+                {
+                    // string.Format ya da normal klasik string birleştirme (+) yöntemi ile sorgumuzu hazırlıyoruz.
+                    string query = string.Format("SELECT * FROM Users WHERE Username='{0}' AND Password='{1}'", txtUser.Text, txtPass.Text);
+
+                    MessageBox.Show($"Sorulan sorgu şu şekildedir :\r\n{query}");
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader sqlDataReader = cmd.ExecuteReader())
+                        {
+                            if (sqlDataReader.Read())
+                            {
+                                MessageBox.Show($"Kayıt Bulundu\r\nId : {sqlDataReader["Id"]}\r\nUsername : {sqlDataReader["Username"]}\r\n{sqlDataReader["Password"]}\r\n{sqlDataReader["Email"]}");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Herhangi bir kayıt bulunamadı");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // SqlParameter kullanarak sorgumuzu hazırlıyoruz.
+                    string query = "SELECT * FROM Users WHERE Username=@Username AND Password=@Password";
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        // Parametrelere değer atıyoruz.
+                        cmd.Parameters.AddWithValue("@Username", txtUser.Text);
+                        cmd.Parameters.AddWithValue("@Password", txtPass.Text);
+
+                        using (SqlDataReader sqlDataReader = cmd.ExecuteReader())
+                        {
+                            if (sqlDataReader.Read())
+                            {
+                                MessageBox.Show($"Kayıt Bulundu\r\nId : {sqlDataReader["Id"]}\r\nUsername : {sqlDataReader["Username"]}\r\n{sqlDataReader["Password"]}\r\n{sqlDataReader["Email"]}");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Herhangi bir kayıt bulunamadı");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata :\r\n{ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Global olarak tanımladığımız SqlConnection nesnemiz null değilse ve açıksa kapatıp dispose edip null a çekiyoruz ki açık kalıp kaynak tüketmesin. Yukarıdaki diğer SqlConnection, SqlCommand gibi unmanagement nesneler için using kullanmıştık. Using kullanımı ile ilgili github reposundaki UsingKullanimi projsine bakabilirsiniz.
+            if (connection != null)
+            {
+                if (connection.State != System.Data.ConnectionState.Closed)
+                    connection.Close();
+                connection.Dispose();
+            }
+            connection = null;
+        }
+    }
+}
