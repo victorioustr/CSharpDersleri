17f4589d2c00aea6e6ef21d435d6fb3139186eb9
victorioustr <makyil@gmail.com>
2019-03-09 22:45:32 +0300

    DecimalListViewFormatting

---
+++ DecimalListViewFormatting/DecimalListViewFormatting/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DecimalListBoxFormatting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(385, 425);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ DecimalListViewFormatting/DecimalListViewFormatting/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Forms;
+
+namespace DecimalListBoxFormatting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BindData();
+        }
+
+        private void BindData()
+        {
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Ondalıksız Para");
+            listView1.Columns.Add("Ondalıksız Sayı");
+            listView1.Columns.Add("2 Ondalıklı Sayı");
+
+            for (int i = 0; i < 100; i++)
+            {
+                decimal deger = (decimal)i * 100000m / 9m;
+
+                ListViewItem item = new ListViewItem(new[] {
+                    string.Format("{0:c0}", deger),
+                    string.Format("{0:n0}", deger),
+                    string.Format("{0:n2}", deger)
+                });
+
+                listView1.Items.Add(item);
+            }
+
+            // Daha fazla format'lama kodları için şurayı ziyaret edebilirsiniz.
+            // https://docs.microsoft.com/tr-tr/dotnet/standard/base-types/standard-numeric-format-strings?view=netframework-4.7.2
+        }
+    }
+}
