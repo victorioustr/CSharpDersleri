e08b153cb806263bf368522849ab509c35530c34
Muzaffer AKYIL <makyil@gmail.com>
2019-06-23 01:41:01 +0300

    Countdown

---
+++ Countdown/Countdown/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Countdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGeriSayimSuresi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblKalanSure = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtGeriSayimSuresi
+            // 
+            this.txtGeriSayimSuresi.Location = new System.Drawing.Point(102, 12);
+            this.txtGeriSayimSuresi.Name = "txtGeriSayimSuresi";
+            this.txtGeriSayimSuresi.Size = new System.Drawing.Size(100, 20);
+            this.txtGeriSayimSuresi.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gerisayım Süresi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "dk";
+            // 
+            // lblKalanSure
+            // 
+            this.lblKalanSure.Location = new System.Drawing.Point(15, 125);
+            this.lblKalanSure.Name = "lblKalanSure";
+            this.lblKalanSure.Size = new System.Drawing.Size(330, 38);
+            this.lblKalanSure.TabIndex = 3;
+            this.lblKalanSure.Text = "lblKalanSure";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(233, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Geri Sayımı Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(233, 39);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(112, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Geri Sayımı Durdur";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 267);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblKalanSure);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGeriSayimSuresi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGeriSayimSuresi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblKalanSure;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Countdown/Countdown/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Countdown
+{
+    /// <summary>
+    /// Geri Sayım Örneği
+    /// Author : Muzaffer Ali AKYIL 
+    /// Email : muzaffer@akyil.net
+    /// Web : https://muzaffer.akyil.net
+    /// Date : 23.06.2019 01:40
+    /// </summary>
+    public partial class Form1 : Form
+    {
+
+        Timer countdownTimer;
+        DateTime startDate;
+        DateTime endDate;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblKalanSure.Text = string.Empty;
+            lblKalanSure.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold);
+            lblKalanSure.TextAlign = ContentAlignment.MiddleCenter;
+            btnStop.Enabled = false;
+            this.AcceptButton = btnStart;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Geri Sayım Örneği";
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (countdownTimer != null && countdownTimer.Enabled)
+            {
+                if (MessageBox.Show("Geri sayım devam ediyor çıkmak istediğinize emin misiniz ?", "Çıkış Onayı", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    countdownTimer.Stop();
+                    countdownTimer.Dispose();
+                }
+            } else
+            {
+                countdownTimer.Dispose();
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtGeriSayimSuresi.Text, out int geriSayilacakSure) || geriSayilacakSure == 0)
+            {
+                MessageBox.Show("Lütfen sayı giriniz");
+                return;
+            }
+
+            countdownTimer = new Timer()
+            {
+                Interval = 100
+            };
+            countdownTimer.Tick += CountdownTimer_Tick;
+
+            startDate = DateTime.UtcNow;
+            endDate = startDate.AddMinutes(geriSayilacakSure);
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            countdownTimer.Start();
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            lblKalanSure.Text = "İşlem İptal Edildi";
+            countdownTimer.Stop();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan fark = endDate - DateTime.UtcNow;
+            if ((int)fark.TotalSeconds > 0)
+            {
+                lblKalanSure.Text = $"{fark.Minutes} dk. {fark.Seconds} sn.";
+            }
+            else
+            {
+                lblKalanSure.Text = "Süre Tamamlandı";
+                countdownTimer.Stop();
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+            }
+        }
+    }
+}
